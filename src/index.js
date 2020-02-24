c2bddea58e5bf2ff5df15c656834552ae8291eb0
rlaw125 <12490147+rlaw125@users.noreply.github.com>
2020-02-23 21:13:05 -0500

    added some boxes and stylings

--- src/index.js
+++ src/index.js
@@ -4,11 +4,27 @@ import axios from 'axios';
 
 console.log(sum(1,2));
 
+/*
 const main = async () => {
-  const res = await axios.get('https://fizal.me/pokeapi/api/v2/name/bulbasaur.json');
+  const res = await axios.get('https://project.wnyc.org/ice-cream/data/places.json​');
   console.log(res.data);
 }
 
 main();
+*/
+let button=document.getElementById("chatNow");
+let chatBox=document.getElementById("chatbox");
+let sendBox=document.getElementById("sendbox");
 
-
+button.addEventListener("click", function(event){
+  console.log("chatting now");
+  chatBox.style.backgroundColor="lightGray";
+  chatBox.style.height="25vw";
+  chatBox.style.width="32vw";
+  chatBox.style.borderRadius="20%";
+  sendBox.style.backgroundColor="lightGray";
+  sendBox.style.borderStyle="solid";
+  sendbox.style.height="8vw";
+  sendBox.style.width="30vw";
+  sendBox.style.borderRadius="20%";
+});
